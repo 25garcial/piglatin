208a004f513ea1ae0d08c8a5c04be69a9aba3e0b
25garcial <91680716+25garcial@users.noreply.github.com>
2023-10-25 14:07:51 +0000

    modified:   script.js

--- script.js
+++ script.js
@@ -17,18 +17,24 @@ constructor(name){
 
 }
 
-function convertToPiglatin(){
-    
+function convertToPiglatin(english){
+    var consonantGroups= ["sh", "ch", "th"];
+    var consonants=["b","c", "d", "f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","z"];
+    if (english.toString().slice(1) ){
+
+    }
+
 }
 
 function main(){
+
 var input = new textbox();
 submitButton = document.createElement("button");
 var english="";
 submitButton.addEventListener("click", ()=>{
     english=input.value;
     input.value="";
-    convertToPiglatin();
+    convertToPiglatin(english);
 });
 
 }
